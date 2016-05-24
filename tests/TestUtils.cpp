c899ba64492ca51ad63abe728a359290872e4d06
Patrick Lühne <patrick-github@luehne.de>
2016-05-24 02:23:56 +0200

    Added tests covering basic parsing.

--- tests/TestUtils.cpp
+++ tests/TestUtils.cpp
@@ -4,6 +4,21 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+TEST(UtilsTests, ParseSimple)
+{
+	std::stringstream stream("identifier  5   \n-51\t expected unexpected 100 -100");
+
+	ASSERT_EQ(plasp::utils::parse<std::string>(stream), "identifier");
+	ASSERT_EQ(plasp::utils::parse<size_t>(stream), 5);
+	ASSERT_EQ(plasp::utils::parse<int>(stream), -51);
+	ASSERT_NO_THROW(plasp::utils::parseExpected<std::string>(stream, "expected"));
+	ASSERT_THROW(plasp::utils::parseExpected<std::string>(stream, "expected"), plasp::utils::ParserException);
+	ASSERT_NO_THROW(plasp::utils::parseExpected<size_t>(stream, 100));
+	ASSERT_THROW(plasp::utils::parseExpected<size_t>(stream, 100), plasp::utils::ParserException);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 TEST(UtilsTests, EscapeASP)
 {
 	const std::string predicate = "action(stack_on(block-1, block-2, value@3, value@4))";
