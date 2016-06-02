8817a3f2265e44b8f893f51047df2e30ab663c5d
Patrick Lühne <patrick-github@luehne.de>
2016-06-02 22:47:25 +0200

    Fixed whitespace parsing issue.

--- src/plasp/pddl/Action.cpp
+++ src/plasp/pddl/Action.cpp
@@ -55,6 +55,8 @@ Action &Action::parseDeclaration(utils::Parser &parser, Context &context)
 			action->m_precondition = parsePreconditionExpression(parser, context, action->m_parameters);
 		else if (sectionIdentifier == "effect")
 			action->m_effect = parseEffectExpression(parser, context, action->m_parameters);
+
+		parser.skipWhiteSpace();
 	}
 
 	// Store new action
