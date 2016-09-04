dd621fcd5c19ecf61d23d4461e4d09772e20e638
Patrick Lühne <patrick-github@luehne.de>
2016-09-04 23:54:15 +0200

    Minor clean-up.

--- src/plasp/pddl/expressions/Imply.cpp
+++ src/plasp/pddl/expressions/Imply.cpp
@@ -30,18 +30,13 @@ ExpressionPointer Imply::simplified()
 	m_arguments[1] = m_arguments[1]->simplified();
 
 	auto notArgument0 = NotPointer(new Not);
-	notArgument0->setArgument(std::move(m_arguments[0]));
+	notArgument0->setArgument(m_arguments[0]);
 
 	auto orExpression = OrPointer(new Or);
-	orExpression->addArgument(std::move(notArgument0));
-	orExpression->addArgument(std::move(m_arguments[1]));
+	orExpression->addArgument(notArgument0);
+	orExpression->addArgument(m_arguments[1]);
 
-	auto normalizedOrExpression = orExpression->simplified();
-
-	if (normalizedOrExpression)
-		return normalizedOrExpression;
-
-	return std::move(orExpression);
+	return orExpression->simplified();
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
