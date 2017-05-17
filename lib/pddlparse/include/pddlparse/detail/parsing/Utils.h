29f8ee07784382a34171129bcd92828a86c8ac1f
Patrick Lühne <patrick-github@luehne.de>
2017-05-17 15:28:58 +0200

    Added parsing utils to skip PDDL sections.

---
+++ lib/pddlparse/include/pddlparse/detail/parsing/Utils.h
@@ -0,0 +1,43 @@
+#ifndef __PDDL_PARSE__DETAIL__PARSE_UTILS_H
+#define __PDDL_PARSE__DETAIL__PARSE_UTILS_H
+
+#include <pddlparse/Tokenizer.h>
+
+namespace pddl
+{
+namespace detail
+{
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// ParseUtils
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+inline void skipSection(Tokenizer &tokenizer)
+{
+	size_t openParentheses = 1;
+
+	while (true)
+	{
+		const auto character = tokenizer.currentCharacter();
+		tokenizer.advance();
+
+		if (character == '(')
+			openParentheses++;
+		else if (character == ')')
+		{
+			openParentheses--;
+
+			if (openParentheses == 0)
+				return;
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
+}
+
+#endif
