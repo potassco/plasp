6d07fcbd6c514146f60cc6a4ebb886e609d2234d
Patrick Lühne <patrick-github@luehne.de>
2017-10-27 16:36:01 +0200

    Moved initial state translation to new header.

--- src/plasp/pddl/TranslatorASP.cpp
+++ src/plasp/pddl/TranslatorASP.cpp
@@ -11,6 +11,7 @@
 #include <plasp/pddl/translation/DerivedPredicate.h>
 #include <plasp/pddl/translation/DerivedPredicatePrecondition.h>
 #include <plasp/pddl/translation/Effect.h>
+#include <plasp/pddl/translation/Fact.h>
 #include <plasp/pddl/translation/Goal.h>
 #include <plasp/pddl/translation/Precondition.h>
 #include <plasp/pddl/translation/Predicate.h>
@@ -436,51 +437,8 @@ void TranslatorASP::translateInitialState() const
 
 	const auto &facts = m_description.problem.value()->initialState.facts;
 
-	// TODO: move to separate header
 	for (const auto &fact : facts)
-	{
-		m_outputStream << std::endl << colorlog::Function("initialState") << "(";
-
-		const auto handlePredicate =
-			[&](const ::pddl::normalizedAST::PredicatePointer &predicate, bool isPositive = true)
-			{
-				translatePredicateToVariable(m_outputStream, *predicate, isPositive);
-			};
-
-		const auto handleNegatedPredicate =
-			[&](const ::pddl::normalizedAST::PredicatePointer &predicate)
-			{
-				return handlePredicate(predicate, false);
-			};
-
-		const auto handleDerivedPredicate =
-			[&](const ::pddl::normalizedAST::DerivedPredicatePointer &, bool = true)
-			{
-				throw TranslatorException("derived predicates should not occur in initial state");
-			};
-
-		const auto handleNegatedDerivedPredicate =
-			[&](const ::pddl::normalizedAST::DerivedPredicatePointer &derivedPredicate)
-			{
-				return handleDerivedPredicate(derivedPredicate, false);
-			};
-
-		const auto handleAtomicFormula =
-			[&](const ::pddl::normalizedAST::AtomicFormula &atomicFormula)
-			{
-				atomicFormula.match(handlePredicate, handleDerivedPredicate);
-			};
-
-		const auto handleNot =
-			[&](const ::pddl::normalizedAST::NotPointer<::pddl::normalizedAST::AtomicFormula> &not_)
-			{
-				not_->argument.match(handleNegatedPredicate, handleNegatedDerivedPredicate);
-			};
-
-		fact.match(handleAtomicFormula, handleNot);
-
-		m_outputStream << ").";
-	}
+		::plasp::pddl::translateFact(m_outputStream, fact);
 
 	m_outputStream
 		<< std::endl << std::endl
